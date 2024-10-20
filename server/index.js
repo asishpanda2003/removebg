166f8c21d6e2a41369dfb26390f41d0aa16c8219
Asish kumar Panda <112423732+asishpanda2003@users.noreply.github.com>
2024-10-20 21:14:42 +0530

    update index file

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import 'dotenv/config'
 import cors from "cors"
 import connectDB from "./configs/mongoDB.js";
+import userRouter from "./routes/userRoutes.js";
 
 //App config
 const PORT=process.env.PORT || 4000;
@@ -16,6 +17,8 @@ app.use(cors())
 app.get('/',(req,res)=>{
     res.send('Hello World');
 })
+app.use('/api/user',userRouter)
+
 
 app.listen(PORT,()=>{
     console.log("Server is Running "+PORT);
